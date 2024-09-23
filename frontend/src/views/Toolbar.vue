b112eeb14ba4d18fd737b2c144f487d7e88b683e
thidtanai <thidsloth@gmail.com>
2024-09-23 13:14:28 +0700

    Style Toolbar: change row col

--- frontend/src/views/Toolbar.vue
+++ frontend/src/views/Toolbar.vue
@@ -8,34 +8,32 @@
       app
       class="px-16"
     >
-      <!-- Left -->
-      <v-row class="ma-0 rows pl-10">
+      <v-row class="rows px-6">
+        <!-- Left -->
         <v-col class="pa-0 d-flex flex-col justify-start align-center">
-          <v-icon>mdi-menu</v-icon>
+          <v-btn depressed color="white" class="menu-btn">
+            <v-icon color="#008ECC">mdi-menu</v-icon>
+          </v-btn>
           <p class="ma-0 ml-4">Lotto 888</p>
         </v-col>
-      </v-row>
 
-      <!-- Middle -->
-      <v-row class="ma-0 rows pa-0">
-        <v-col class="pa-0">
-          <v-icon>mdi-magnify</v-icon>
-        </v-col>
-      </v-row>
-
-      <!-- Right -->
-      <v-row class="ma-0 rows pr-10">
-        <v-col cols="10" class="pa-0 d-flex align-center justify-end pr-3">
-          <v-icon>mdi-account-outline</v-icon>
-          <p class="ma-0 ml-2">Sign Up/Sign In</p>
-        </v-col>
-        <v-divider vertical></v-divider>
-        <v-col cols="2" class="pa-0 d-flex align-center justify-end">
-          <v-icon>mdi-cart-outline</v-icon>
-          <p class="ma-0 ml-2">Cart</p>
+        <!-- Right -->
+        <v-col class="pa-0 d-flex align-center justify-end">
+          <!-- Search bar -->
+          <v-icon class="mr-7">mdi-magnify</v-icon>
+          <v-btn depressed color="white" class="menu-btn">
+            <v-icon color="#008ECC">mdi-account-outline</v-icon>
+            <p class="ma-0 ml-2">Sign Up/Sign In</p>
+          </v-btn>
+          <v-divider vertical class="mr-1 ml-1"></v-divider>
+          <v-btn depressed color="white" class="menu-btn">
+            <v-icon color="#008ECC">mdi-cart-outline</v-icon>
+            <p class="ma-0 ml-2">Cart</p>
+          </v-btn>
         </v-col>
       </v-row>
     </v-app-bar>
+    <v-divider class="mr-1 ml-1"></v-divider>
     <!-- Content -->
     <v-container>
       <router-view />
@@ -51,5 +49,10 @@ export default {};
 .rows {
   width: 100%;
   margin: 0 !important;
+}
+
+.menu-btn {
+  text-transform: none; /* Prevent uppercase transformation */
+  font-weight: inherit; /* Retain the font weight */
 }
 </style>
