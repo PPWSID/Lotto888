5582bccbd6d11de9daf8a07361120a61c392850d
thidtanai <thidsloth@gmail.com>
2024-09-30 13:38:17 +0700

    perf: :safety_vest: add email validation
    edit user model to required email and add validation to register path

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userschema = new Schema({
-        user_id : {type : String ,Unique : true},
-        username : {type : String},
-        password : {type : String},
-        gmail : {type : String , require : true},
-        tel : {type : String},
-        birth : {type : Date},
-        age : {type : String},
-        sex :{type : String},
-    },
-    {
-        timestamps: true
-    }
+const userschema = new Schema(
+  {
+    user_id: { type: String, Unique: true },
+    username: { type: String },
+    password: { type: String },
+    gmail: { type: String, unique: true, require: true },
+    tel: { type: String },
+    birth: { type: Date },
+    age: { type: String },
+    sex: { type: String },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const userModel = mongoose.model('userData',userschema,'userData')
+const userModel = mongoose.model("userData", userschema, "userData");
 
-module.exports = userModel
+module.exports = userModel;
